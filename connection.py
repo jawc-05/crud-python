774165c5709e33e94127b10d6b5516b47a6a780b
jawc-05 <joaoalfredowc@gmail.com>
2025-09-29 16:26:04 -0300

    feat: adding connection.py

--- connection.py
+++ connection.py
@@ -1,2 +1,14 @@
 from getpass import getpass
-from mysql.connector import connect
+from mysql.connector import connect, Error
+
+def create_connection(action):
+    try:
+        with connect(
+            host = "localhost",
+            user = input("Digite o usuário do banco de dados: "),
+            password = getpass("Digite a senha do banco de dados: "),
+        ) as connection:
+            print("Conexão bem sucedida!", connection)
+            return action(connection)
+    except Error as e:
+        print("Ocorreu um erro ao tentar se conectar ao banco de dados.")

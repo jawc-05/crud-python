e11046eb64c826c66bc3efc814948649d7c17b37
jawc-05 <joaoalfredowc@gmail.com>
2025-09-30 15:57:33 -0300

    docs: removing blank spaces

--- connection.py
+++ connection.py
@@ -39,16 +39,3 @@ class ConnectionManager:
             print(f"\n Erro ao conectar ao MySQL (DB: {db_to_connect}): {e}")
             self.password = None 
             return None
-
-
-def create_connection(action):
-    try:
-        with connect(
-            host = "localhost",
-            user = input("Digite o usuário do banco de dados: "),
-            password = getpass("Digite a senha do banco de dados: "),
-        ) as connection:
-            print("Conexão bem sucedida!", connection)
-            return action(connection)
-    except Error as e:
-        print("Ocorreu um erro ao tentar se conectar ao banco de dados.")

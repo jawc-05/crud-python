57ce618ca558b77d2fb3e6c265d95fe49d94bec9
jawc-05 <joaoalfredowc@gmail.com>
2025-09-30 19:29:48 -0300

    feat: adding the right imports to read

--- operations/read.py
+++ operations/read.py
@@ -1,2 +1,6 @@
+from mysql.connector import Error
+from connection import db_manager
+from settings import TABLE_NAME
+
 def read_user():
     print("⚠️ UPDATE: Função em desenvolvimento. Tente as opções 0, 1 ou 2.")

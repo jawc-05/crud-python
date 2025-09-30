df24ba76ea56c83da0dafcc6c2c62ca4eedff970
jawc-05 <joaoalfredowc@gmail.com>
2025-09-30 16:27:29 -0300

    feat: adding setup_database

--- operations/setup_read.py
+++ operations/setup_read.py
@@ -2,3 +2,9 @@ from mysql.connector import Error
 from connection import db_manager 
 from settings import TABLE_NAME, DB_NAME 
 
+def setup_database():
+    
+    connection = db_manager.connect_only_server()
+    if not connection:
+        print("Não foi possível iniciar o setup. Verifique as credenciais e tente novamente.")
+        return

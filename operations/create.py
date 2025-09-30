fa246bb612ca9ae647539d8c105357ddca41a4e3
jawc-05 <joaoalfredowc@gmail.com>
2025-09-30 16:50:15 -0300

    feat: adding create_user

--- operations/create.py
+++ operations/create.py
@@ -1,3 +1,16 @@
 from mysql.connector import Error
 from connection import db_manager
 from settings import TABLE_NAME
+
+def create_user():
+
+    print("\n ---CADASTRAR NOVO USUÁRIO---")
+    cpf = input("Digite o CPF (PK): ")
+    nome = input("Digite o nome: ")
+    email = input("Digite o email: ")
+
+    query = f""""
+    INSERT INTO {TABLE_NAME} (cpf, nome, email)
+    VALUES (%s, %s, %s)
+    """
+    data = (cpf, nome, email)

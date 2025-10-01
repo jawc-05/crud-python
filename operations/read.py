374775f539d6ce3bdbf924609cd459057a30b73a
jawc-05 <joaoalfredowc@gmail.com>
2025-09-30 22:57:48 -0300

    docs: adding comments do read

--- operations/read.py
+++ operations/read.py
@@ -3,21 +3,26 @@ from database.connection import db_manager
 from configs.settings import TABLE_NAME
 
 def read_user():
+    #ESSA FUNCAO EXECUTA UM READ DA TABLE INTEIRA(usuarios)
     query = f"SELECT cpf, nome, email FROM {TABLE_NAME}"
     
+    #ESTABELE A CONEXAO USANDO A INSTANCIA POO DA CAMADA database
     connection = db_manager.create_connection()
     if not connection: return
 
     with connection:
         with connection.cursor() as cursor:
             try:
+                #EXECUTA A CONSULTA
                 cursor.execute(query)
 
+                #RECUPERA TODOS OS DADOS DO BANCO DE DADOS E OS COLOCA EM UMA LISTA
                 users = cursor.fetchall()
 
                 if not users:
                     print("\n Nenhum usuário encontrado na tabela '{TABLE_NAME}', cadastre um novo usuário.")
 
+                #EXIBE DE FORMA FORMATADA
                 print("\n" + "=" * 77)
                 print("--- LISTA DE USUÁRIOS ---")
                 print(f"{'CPF':<15} | {'Nome':<30} | {'Email':<30}")
@@ -28,4 +33,5 @@ def read_user():
                 print("=" * 77 + "\n")
             
             except Error as e:
+                #TRATA OS ERROS DO SQL
                 print(f"\n Erro ao ler usuários: {e}")
